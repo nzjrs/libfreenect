cea6f81395ef8721dfd3bade920ce3a9e031afa7
John Stowers <john.stowers@gmail.com>
2011-08-18 21:10:53 +1200

    fix cpp link with cvsync

--- wrappers/opencv/libfreenect_cv.h
+++ wrappers/opencv/libfreenect_cv.h
@@ -1,11 +1,11 @@
 #ifndef FREENECT_CV_H
 #define FREENECT_CV_H
 
+#include <opencv/cv.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#include <opencv/cv.h>
 
 	IplImage *freenect_sync_get_depth_cv(int index);
 	IplImage *freenect_sync_get_rgb_cv(int index);
